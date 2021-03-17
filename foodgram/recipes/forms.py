8ea6c246ce4581064c4999a6802fc2055cddc05f
Nametkin <aleksei.nametkin@accenture.com>
2021-03-17 22:27:44 +0300

    sdgsg

--- foodgram/recipes/forms.py
+++ foodgram/recipes/forms.py
@@ -1,14 +1,25 @@
 from django import forms
 
-from .models import Recipe
+from .models import Ingredient, Recipe
+
+
+class IngredientForm(forms.ModelForm):
+    class Meta:
+        model = Ingredient
+        exclude = ('recipe',)
 
 
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
         exclude = ('author',)
-        labels = {'title': 'Название рецепта',
-                  'tags': 'Теги',
-                  'ingredients': 'Ингредиенты',
-                  'time_for_preparing': 'Время приготовления',
-                  'text': 'Описание'}
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form__input'}),
+        }
+        labels = {
+            'title': 'Название рецепта',
+            'tags': 'Теги',
+            'ingredients': 'Ингредиенты',
+            'time_for_preparing': 'Время приготовления',
+            'text': 'Описание'
+        }
