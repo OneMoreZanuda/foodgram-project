6a786a4d794922df469eb1fa7743351ca59324d3
Nametkin <aleksei.nametkin@accenture.com>
2021-03-18 18:38:46 +0300

    tags filter

--- foodgram/recipes/templatetags/user_filters.py
+++ foodgram/recipes/templatetags/user_filters.py
@@ -4,12 +4,24 @@ from urllib.parse import urlencode
 register = template.Library()
 
 
-@register.simple_tag
-def updated_params(dict_, operation, **kwargs):
-    dict_copy = dict_.copy()
-    if operation == 'update':
-        dict_copy.update(kwargs)
-    elif operation == 'remove':
-        for key in kwargs:
-            dict_copy.pop(kwargs, '')
-    return urlencode(dict_copy)
+@register.inclusion_tag("includes/tag_item.html", takes_context=True)
+def tag_item(context):
+    name = context['name']
+    checked = context[name]
+
+    query_params = context['request'].GET
+    new_query_params = query_params.copy()
+    new_query_params.pop(name, '')
+    if checked:
+        new_query_params[name] = 'no'
+        
+    new_query_string = urlencode(new_query_params)
+    link = './' if new_query_string else f'?{new_query_string}'
+
+    return {
+        'name': name,
+        'checked': checked,
+        'link': link,
+        'label': context['label'],
+        'color': context['color']
+    }
--- foodgram/recipes/views.py
+++ foodgram/recipes/views.py
@@ -1,11 +1,9 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView
 
 from .models import Recipe
 from .utils import CachedPaginator
-
-# def index(request):
-#     return render(request, 'recipes/indexNotAuth.html', context={})
 
 
 class Index(ListView):
@@ -14,6 +12,19 @@ class Index(ListView):
     template_name = 'recipes/indexNotAuth.html'
     paginator_class = CachedPaginator
     paginate_by = 6
+
+    def get_context_data(self, **kwargs):
+        data = super(Index, self).get_context_data(**kwargs)
+        query_params = self.request.GET
+        for tag_name, _ in Recipe.tags.field.choices:
+            if tag_name not query_params:
+                tag_checked = True
+            elif query_params[tag_name] == 'no':
+                tag_checked = False
+            else:
+                raise Http404(f'Invalid value of the parameter "{tag_name}"')
+            data[tag_name] = tag_checked
+        return data
 
 
 class CreateRecipeView(CreateView):
