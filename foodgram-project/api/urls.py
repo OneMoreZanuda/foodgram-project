b967e10c52b41a91d348c1cb42e931f1be877d16
Nametkin <aleksei.nametkin@accenture.com>
2021-04-25 00:00:30 +0300

    delete commented code

--- foodgram-project/api/urls.py
+++ foodgram-project/api/urls.py
@@ -1,43 +1,37 @@
 from django.urls import path
 
-# from rest_framework.routers import DefaultRouter
-
 from . import views
-
-# router = DefaultRouter()
-# router.register('favorites', views.FavoritesViewSet, basename='favorites')
-
 
 urlpatterns = [
     path('products/', views.get_products, name='get_products'),
     path(
         'favorites/',
         views.add_to_favorites,
-        name='add_to_favorites'
+        name='add_to_favorites',
     ),
     path(
         'favorites/<int:recipe_id>',
         views.remove_from_favorites,
-        name='remove_from_favorites'
+        name='remove_from_favorites',
     ),
     path(
         'subscriptions/',
         views.add_to_subscriptions,
-        name='add_to_subscriptions'
+        name='add_to_subscriptions',
     ),
     path(
         'subscriptions/<int:author_id>',
         views.remove_from_subscriptions,
-        name='remove_from_subscriptions'
+        name='remove_from_subscriptions',
     ),
     path(
         'purchases/',
         views.add_to_purchases,
-        name='add_to_purchases'
+        name='add_to_purchases',
     ),
     path(
         'purchases/<int:recipe_id>',
         views.remove_from_purchases,
-        name='remove_from_purchases'
-    )
+        name='remove_from_purchases',
+    ),
 ]
